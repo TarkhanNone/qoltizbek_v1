528213d37ca3dc2c82fc700f3ea0c886a7b9438a
darkhan_zhaksy <wwwdarkhan96@gmail.com>
2026-04-29 19:18:27 +0500

    feat: #2 add custom User model with phone, avatar, position fields

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
 
-# Register your models here.
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    list_display = ['username', 'email', 'first_name', 'last_name', 'position']
+    fieldsets = UserAdmin.fieldsets + (
+        ('Дополнительно', {'fields': ('phone', 'avatar', 'position')}),
+    )
